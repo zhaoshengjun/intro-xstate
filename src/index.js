1e24d62bcc3bd03f9abdb2095fbfa7e8cac4f144
Sam Zhao <zhao.shengjun@gmail.com>
2020-01-06 20:23:35 +1000

    use xstate

--- src/index.js
+++ src/index.js
@@ -1,38 +1,26 @@
-const STATES = {
-	lit: "lit",
-	unlit: "unlit",
-	broken: "broken"
-};
+import { Machine } from "xstate";
 
-function lightBulb() {
-	let state = STATES.unlit;
-
-	return {
-		state() {
-			return state;
-		},
-
-		toggle() {
-			switch (state) {
-				case STATES.lit:
-					state = STATES.unlit;
-					break;
-				case STATES.unlit:
-					state = STATES.lit;
-					break;
+const lightBubleMachine = Machine({
+	id: "lightBuble",
+	initial: "unlit",
+	states: {
+		lit: {
+			on: {
+				BREAK: "broken",
+				TOGGLE: "unlit"
 			}
 		},
+		unlit: {
+			on: {
+				BREAK: "broken",
+				TOGGLE: "lit"
+			}
+		},
+		broken: {}
+	}
+});
 
-		break() {
-			state = STATES.broken;
-		}
-	};
-}
-
-const buble = lightBulb();
-const log = () => {
-	console.log(buble.state());
-};
-buble.toggle();
-buble.break();
-log();
+console.log(lightBubleMachine.initialState.value);
+console.log(lightBubleMachine.transition("unlit", "TOGGLE").value);
+console.log(lightBubleMachine.transition("unlit", "BREAK").value);
+console.log(lightBubleMachine.transition("broken", "TOGGLE").value);
