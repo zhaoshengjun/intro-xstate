19216b4ad3032370e2dfb5f663d2dc9c53292a3d
Sam Zhao <zhao.shengjun@gmail.com>
2020-01-07 22:18:39 +1000

    send data with action

--- src/index.js
+++ src/index.js
@@ -30,4 +30,4 @@ const service = interpret(lightBubleMachine).start("lit");
 service.onTransition(state => console.log(state.value));
 service.send("TOGGLE");
 service.send("TOGGLE");
-service.send("BREAK");
+service.send("BREAK", { room: "dinning room" });
