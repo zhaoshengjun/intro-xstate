c6685f3f18a2c894fc28072820f0829a71d43e94
Sam Zhao <zhao.shengjun@gmail.com>
2020-01-08 22:02:24 +1000

    new idle machine

--- src/index.js
+++ src/index.js
@@ -1,41 +1,26 @@
 import { Machine, interpret } from "xstate";
 
-const lightBubleMachine = Machine(
+const idleMachine = Machine(
 	{
-		id: "lightBuble",
-		initial: "unlit",
+		id: "idle",
+		initial: "idle",
 		states: {
-			lit: {
-				exit: ["darkAndCold"],
-				on: {
-					BREAK: "broken",
-					TOGGLE: "unlit"
-				}
-			},
-			unlit: {
-				on: {
-					BREAK: "broken",
-					TOGGLE: "lit"
-				}
-			},
-			broken: {
-				entry: ["logBroken"]
+			idle: {
+				entry: ["logEntry"],
+				exit: ["logExit"]
 			}
+		},
+		on: {
+			DO_NOTHING: "idle"
 		}
 	},
 	{
 		actions: {
-			logBroken: (context, event) => {
-				console.log({ context, event });
-			},
-			darkAndCold: () => console.log("It's getting dark and cold.")
+			logEntry: () => console.log("entered"),
+			logExit: () => console.log("exited")
 		}
 	}
 );
 
-const service = interpret(lightBubleMachine).start("lit");
-
-service.onTransition(state => console.log(state.value));
-service.send("TOGGLE");
-service.send("TOGGLE");
-service.send("BREAK", { room: "dinning room" });
+const service = interpret(idleMachine).start();
+service.send("DO_NOTHING");
