bf905462e5fbd37e926d35a2b4f1a9fc32d0c3c0
Sam Zhao <zhao.shengjun@gmail.com>
2020-01-10 23:17:50 +1000

    use activity to run functions during the whole state

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
 import { Machine, interpret, assign } from "xstate";
 
-const alarmClockMachine = Machine({
-	id: "alarmClock",
-	initial: "idle",
-	states: {
-		idle: {
-			on: {
-				ALARM: "alarming"
+const alarmClockMachine = Machine(
+	{
+		id: "alarmClock",
+		initial: "idle",
+		states: {
+			idle: {
+				on: {
+					ALARM: "alarming"
+				}
+			},
+			alarming: {
+				activities: ["beeping"],
+				on: {
+					STOP: "idle"
+				}
 			}
-		},
-		alarming: {
-			on: {
-				STOP: "idle"
+		}
+	},
+	{
+		activities: {
+			beeping: () => {
+				const beep = () => console.log("beep");
+				beep();
+				const interval = setTimeout(beep, 1000);
+				return () => clearInterval(interval);
 			}
 		}
 	}
-});
+);
 
 const service = interpret(alarmClockMachine).start();
 console.log(service.state.value);
 service.send("ALARM");
 console.log(service.state.value);
-service.send("STOP");
-console.log(service.state.value);
+setTimeout(() => {
+	service.send("STOP");
+	console.log(service.state.value);
+}, 2000);
